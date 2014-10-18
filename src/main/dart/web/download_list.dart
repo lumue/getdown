15d1122b89182c7f549df3e5866b12fce29d6e05
lm <mueller.lutz@gmail.com>
2014-10-18 02:37:48 +0200

    work on download_list component

--- src/main/dart/web/download_list.dart
+++ src/main/dart/web/download_list.dart
@@ -8,7 +8,16 @@ class DownloadList extends PolymerElement {
  
   @observable List<Download> downloads = toObservable([]);
 
-  DownloadList.created() : super.created();
+  DownloadList.created() : super.created()
+  {
+    downloads.add(new Download("file 1.zip",20000,5000));
+    downloads.add(new Download("file 2.zip",20000,3000));
+    downloads.add(new Download("file 3.zip",20000,8000));
+    downloads.add(new Download("file 4.zip",20000,10000));
+    downloads.add(new Download("file 5.zip",20000,9000));
+    downloads.add(new Download("file 6.zip",20000,5000));
+    downloads.add(new Download("file 7.zip",20000,1000));
+  }
 
  
   
