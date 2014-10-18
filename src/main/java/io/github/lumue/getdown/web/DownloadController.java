14fbe5ed107e0583cd4a766bcf0de076bdb8e12a
lm <mueller.lutz@gmail.com>
2014-10-18 04:34:35 +0200

    re added java code

---
+++ src/main/java/io/github/lumue/getdown/web/DownloadController.java
@@ -0,0 +1,31 @@
+package io.github.lumue.getdown.web;
+
+
+import io.github.lumue.getdown.application.DownloadJob.DownloadJobHandle;
+import io.github.lumue.getdown.application.DownloadService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DownloadController {
+
+	@Autowired
+	DownloadService downloadService;
+
+	@RequestMapping("/download/add")
+	public String addDownload(@RequestParam(value = "url", required = true) String url) {
+		return downloadService.addDownload(url).toString();
+	}
+
+	@RequestMapping("/download/start")
+	public void startDownload(@RequestParam(value = "handle", required = true) String handle) {
+		downloadService.startDownload(DownloadJobHandle.create(handle));
+	}
+
+	
+
+
+}
