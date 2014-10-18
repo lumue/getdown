28bf532201b8b2472736b982688699bede7798be
lm <mueller.lutz@gmail.com>
2014-10-18 18:23:40 +0200

    set handle in test download list

--- src/main/dart/web/model.dart
+++ src/main/dart/web/model.dart
@@ -26,11 +26,11 @@ class DownloadRequest extends Observable {
 }
 
 class TestData{
-  static final List<DownloadViewItem> TESTLIST=[new DownloadViewItem("file 1.zip","http://testhost/download/file1.zip",20000,5000),
-                                                  new DownloadViewItem("file 2.zip","http://testhost/download/file1.zip",20000,3000),
-                                                  new DownloadViewItem("file 3.zip","http://testhost/download/file1.zip",20000,8000),
-                                                  new DownloadViewItem("file 4.zip","http://testhost/download/file1.zip",20000,10000),
-                                                  new DownloadViewItem("file 5.zip","http://testhost/download/file1.zip",20000,9000),
-                                                  new DownloadViewItem("file 6.zip","http://testhost/download/file1.zip",20000,5000),
-                                                  new DownloadViewItem("file 7.zip","http://testhost/download/file1.zip",20000,1000)];
+  static final List<DownloadViewItem> TESTLIST=[new DownloadViewItem("X","file 1.zip","http://testhost/download/file1.zip",20000,5000),
+                                                  new DownloadViewItem("X","file 2.zip","http://testhost/download/file1.zip",20000,3000),
+                                                  new DownloadViewItem("X","file 3.zip","http://testhost/download/file1.zip",20000,8000),
+                                                  new DownloadViewItem("X","file 4.zip","http://testhost/download/file1.zip",20000,10000),
+                                                  new DownloadViewItem("X","file 5.zip","http://testhost/download/file1.zip",20000,9000),
+                                                  new DownloadViewItem("X","file 6.zip","http://testhost/download/file1.zip",20000,5000),
+                                                  new DownloadViewItem("X","file 7.zip","http://testhost/download/file1.zip",20000,1000)];
 }
