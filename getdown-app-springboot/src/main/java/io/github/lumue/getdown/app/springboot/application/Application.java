185c7813440b2ad8253948e983adb37ba1939647
lm <mueller.lutz@gmail.com>
2015-06-20 18:51:38 +0200

    seperate configuration from application class

--- getdown-app-springboot/src/main/java/io/github/lumue/getdown/app/springboot/application/Application.java
+++ getdown-app-springboot/src/main/java/io/github/lumue/getdown/app/springboot/application/Application.java
@@ -1,67 +1,16 @@
 package io.github.lumue.getdown.app.springboot.application;
 
-import io.github.lumue.getdown.core.download.job.AsyncDownloadJobRunner;
-import io.github.lumue.getdown.core.download.job.DownloadJobRepository;
-import io.github.lumue.getdown.core.download.job.DownloadService;
-import io.github.lumue.getdown.core.download.job.FilePersistentDownloadJobRepository;
-import io.github.lumue.getdown.core.download.resolver.ContentLocationResolverRegistry;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-import reactor.Environment;
-import reactor.bus.EventBus;
-import reactor.spring.context.config.EnableReactor;
 
 @ComponentScan(basePackages = "io.github.lumue.getdown")
 @EnableAutoConfiguration
-@EnableReactor
-@Configuration
-@PropertySource(ignoreResourceNotFound = true, value = "file://${getdown.path.config}/getdown.properties}")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-	@Bean
-	public ExecutorService executorService() {
-		return Executors.newCachedThreadPool();
-	}
 	
-	@Bean
-	public AsyncDownloadJobRunner downloadJobRunner(ExecutorService executorService,
-			ContentLocationResolverRegistry contentLocationResolverRegistry, @Value("${getdown.path.download}") String downloadPath,
-			EventBus eventbus) {
-		return new AsyncDownloadJobRunner(executorService, contentLocationResolverRegistry, downloadPath, eventbus);
-	}
-
-	@Bean
-	public EventBus reactor(Environment env) {
-		return EventBus.create(env);
-	}
-
-	@Bean
-	DownloadService downloadService(DownloadJobRepository downloadJobRepository, AsyncDownloadJobRunner downloadJobRunner) {
-		return new DownloadService(downloadJobRepository, downloadJobRunner);
-	}
-
-	@Bean
-	DownloadJobRepository downloadJobRepository(@Value("${getdown.path.repository}") String repositoryPath) {
-		return new FilePersistentDownloadJobRepository(repositoryPath);
-	}
-
-	@Bean
-	ContentLocationResolverRegistry contentLocationResolverRegistry() {
-		return new ContentLocationResolverRegistry();
-	}
-
 }
---
+++ getdown-app-springboot/src/main/java/io/github/lumue/getdown/app/springboot/application/ApplicationConfiguration.java
@@ -0,0 +1,59 @@
+package io.github.lumue.getdown.app.springboot.application;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import io.github.lumue.getdown.core.download.job.AsyncDownloadJobRunner;
+import io.github.lumue.getdown.core.download.job.DownloadJobRepository;
+import io.github.lumue.getdown.core.download.job.DownloadService;
+import io.github.lumue.getdown.core.download.job.FilePersistentDownloadJobRepository;
+import io.github.lumue.getdown.core.download.resolver.ContentLocationResolverRegistry;
+import reactor.bus.EventBus;
+
+@Configuration
+@PropertySource(ignoreResourceNotFound = true, value = "file://${getdown.path.config}/getdown.properties}")
+public class ApplicationConfiguration {
+	@Bean
+	public ExecutorService executorService() {
+		return Executors.newCachedThreadPool();
+	}
+
+	@Bean
+	public AsyncDownloadJobRunner downloadJobRunner(
+			ExecutorService executorService,
+			ContentLocationResolverRegistry contentLocationResolverRegistry,
+			@Value("${getdown.path.download}") String downloadPath,
+			EventBus eventbus) {
+		return new AsyncDownloadJobRunner(executorService,
+				contentLocationResolverRegistry, downloadPath, eventbus);
+	}
+
+
+	@Bean
+	public DownloadService downloadService(
+			DownloadJobRepository downloadJobRepository,
+			AsyncDownloadJobRunner downloadJobRunner) {
+		return new DownloadService(downloadJobRepository, downloadJobRunner);
+	}
+
+	@Bean
+	public DownloadJobRepository downloadJobRepository(
+			@Value("${getdown.path.repository}") String repositoryPath) {
+		return new FilePersistentDownloadJobRepository(repositoryPath);
+	}
+
+	@Bean
+	public ContentLocationResolverRegistry contentLocationResolverRegistry() {
+		return new ContentLocationResolverRegistry();
+	}
+
+	@Bean
+	public EventBus eventBus() {
+		return EventBus.create();
+	}
+}
