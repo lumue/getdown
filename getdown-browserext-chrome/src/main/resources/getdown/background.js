6e3a2bac2b7452b1618768cbac0527ae3ccef81c
lm <mueller.lutz@gmail.com>
2015-06-23 01:21:40 +0200

    work on options

--- getdown-browserext-chrome/src/main/resources/getdown/background.js
+++ getdown-browserext-chrome/src/main/resources/getdown/background.js
@@ -6,7 +6,7 @@
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
 	var contentUrl=info.linkUrl;
 	var xmlhttp = new XMLHttpRequest();
-	var url = "http://localhost:8001/download/add?url="+encodeURIComponent(contentUrl);
+	var url = "http://192.168.1.105:8001/download/add?url="+encodeURIComponent(contentUrl);
 
 	xmlhttp.onreadystatechange = function() {
 	    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
