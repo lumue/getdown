180efa3c050fe0755a480e4e4bf0ba2314568f02
lm <mueller.lutz@gmail.com>
2015-08-22 00:15:16 +0200

    port download-list component to polymer 1.0 and iron--list

---
+++ getdown-webconsole/src/main/resources/static/webapp/app/elements/download-list/download-list.js
@@ -0,0 +1,124 @@
+Polymer({
+				is: 'download-list',
+
+				properties: {
+					data: {
+						type: Array,
+      					notify: true
+    				}
+    			},
+
+    			ready: function() {
+          			this.data =[
+                  {
+                    "progress" : 15803759,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "RUNNING",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7652"
+
+                  },
+                  {
+                    "progress" : 15803759,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "RUNNING",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7653"
+                  },
+                  {
+                    "progress" : 105799165,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "FINISHED",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7654"
+                  },
+                  {
+                    "progress" : 105799165,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "FINISHED",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7654"
+                  },
+                  {
+                    "progress" : 105799165,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "FINISHED",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7654"
+                  },
+                  {
+                    "progress" : 105799165,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "FINISHED",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7654"
+                  },
+                  {
+                    "progress" : 105799165,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "FINISHED",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7654"
+                  },
+                  {
+                    "progress" : 105799165,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "FINISHED",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7654"
+                  },
+                  {
+                    "progress" : 105799165,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "FINISHED",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7654"
+                  },
+                  {
+                    "progress" : 105799165,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "FINISHED",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7654"
+                  },
+                  {
+                    "progress" : 105799165,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "FINISHED",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7654"
+                  },
+                  {
+                    "progress" : 105799165,
+                    "url" : "http://streamcloud.eu/pjtjmovrp0rc/Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "name" : "Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "message" : "downloading from http://cdn5.streamcloud.eu:8080/5tv74wcrfcoax3ptxzqinbf46kspsq45myare4i2xqypbnbjxkkd2jspy4/video.mp4 to Star.Trek.Enterprise.S02E13.Dawn.German.DVDRip.XviD-c0nFuSed.avi",
+                    "state" : "FINISHED",
+                    "size" : 105799165,
+                    "handle" : "5da30660-d12d-424f-b55f-80fd2a3a7654"
+                  }
+                ];
+        		}
+			});
--- getdown-webconsole/src/main/resources/static/webapp/app/scripts/app.js
+++ getdown-webconsole/src/main/resources/static/webapp/app/scripts/app.js
@@ -40,19 +40,19 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   addEventListener('paper-header-transform', function(e) {
     var appName = document.querySelector('#mainToolbar .app-name');
     var middleContainer = document.querySelector('#mainToolbar .middle-container');
-    var bottomContainer = document.querySelector('#mainToolbar .bottom-container');
+//    var bottomContainer = document.querySelector('#mainToolbar .bottom-container');
     var detail = e.detail;
     var heightDiff = detail.height - detail.condensedHeight;
     var yRatio = Math.min(1, detail.y / heightDiff);
     var maxMiddleScale = 0.50;  // appName max size when condensed. The smaller the number the smaller the condensed size.
     var scaleMiddle = Math.max(maxMiddleScale, (heightDiff - detail.y) / (heightDiff / (1-maxMiddleScale))  + maxMiddleScale);
-    var scaleBottom = 1 - yRatio;
+//    var scaleBottom = 1 - yRatio;
 
     // Move/translate middleContainer
     Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
 
     // Scale bottomContainer and bottom sub title to nothing and back
-    Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
+    //Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
 
     // Scale middleContainer appName
     Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
