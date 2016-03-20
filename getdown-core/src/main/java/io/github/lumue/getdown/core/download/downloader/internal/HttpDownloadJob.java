0172917a3faa52169c56765750d325dcaf427c32
lumue <ddlom85s>
2016-03-20 23:35:08 +0100

    add name and id to downloads

--- getdown-core/src/main/java/io/github/lumue/getdown/core/download/downloader/internal/HttpDownloadJob.java
+++ getdown-core/src/main/java/io/github/lumue/getdown/core/download/downloader/internal/HttpDownloadJob.java
@@ -25,7 +25,7 @@ public class HttpDownloadJob extends AbstractDownloadJob {
 
 	private final static ContentDownloader DOWNLOADER = new HttpContentDownloader();
 
-	private HttpDownloadJob(String url, String outputFilename,String host) {
+	private HttpDownloadJob(String name,String url, String outputFilename,String host) {
 		super(name, url, outputFilename,host);
 	}
 
@@ -80,7 +80,7 @@ public class HttpDownloadJob extends AbstractDownloadJob {
 
 		@Override
 		public DownloadJob build() {
-			return new HttpDownloadJob(this.url, this.outputFilename,this.host);
+			return new HttpDownloadJob(this.name,this.url, this.outputFilename,this.host);
 		}
 
 	}
