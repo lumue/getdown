ed37915a5c6099cade599b1241948812c5829f41
lm <mueller.lutz@gmail.com>
2016-06-24 21:26:00 +0200

    cors für websocket

--- getdown-app-springboot/src/main/java/io/github/lumue/getdown/app/springboot/web/WebSocketConfig.java
+++ getdown-app-springboot/src/main/java/io/github/lumue/getdown/app/springboot/web/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -23,7 +24,9 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").withSockJS();
+		registry.addEndpoint("/ws")
+				.setAllowedOrigins("*")
+				.withSockJS();
 	}
 
 	@Bean
