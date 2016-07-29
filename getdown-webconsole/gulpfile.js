3deeb443d4bc44f5bacaa5706a64db4bfc0f925c
lm <mueller.lutz@gmail.com>
2016-07-29 19:20:06 +0200

    install js build tools from gradle

---
+++ getdown-webconsole/gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var vulcanize = require('gulp-vulcanize');
+
+gulp.task('default', function () {
+	return gulp.src('src/index.html')
+		.pipe(vulcanize({
+			abspath: '',
+			excludes: [],
+			stripExcludes: false
+		}))
+		.pipe(gulp.dest('dest'));
+});
