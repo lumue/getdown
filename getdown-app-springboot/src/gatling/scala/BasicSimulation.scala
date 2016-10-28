5d6b6713c1820be0252834a57a3814d51b92790f
lm <mueller.lutz@gmail.com>
2016-10-29 01:00:54 +0200

    add gatling testing

---
+++ getdown-app-springboot/src/gatling/scala/BasicSimulation.scala
@@ -0,0 +1,31 @@
+import io.gatling.core.Predef._
+import io.gatling.core.feeder.RecordSeqFeederBuilder
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+class BasicSimulation extends Simulation {
+//	val app: ConfigurableApplicationContext = SpringApplication.run(classOf[Application])
+//	Runtime.getRuntime.addShutdownHook(new Thread() {
+//		override def run(): Unit = app.stop()
+//	})
+
+
+	val feeder: RecordSeqFeederBuilder[String] = csv("xhamster-urls.csv")
+
+	val httpConf: HttpProtocolBuilder = http.baseURL("http://localhost:8001")
+
+
+	val scn: ScenarioBuilder = scenario("BasicSimulation")
+		  .repeat(4) {
+			  feed(feeder)
+			    .exec(
+				  http("add ${url}").post("/download").body(StringBody("${url}"))
+			  )
+			  .pause(1)
+		  }
+
+
+	setUp(
+		scn.inject(atOnceUsers(20))
+	).protocols(httpConf)
+}
