9f6881f589253ed81768a787454bac19619592c0
lm <mueller.lutz@gmail.com>
2018-04-24 17:29:24 +0200

    work on buildprocess for html apps

--- getdown-core/src/main/java/io/github/lumue/getdown/core/common/util/ContentFilterEventTap.java
+++ getdown-core/src/main/java/io/github/lumue/getdown/core/common/util/ContentFilterEventTap.java
@@ -7,42 +7,43 @@ import java.util.function.Predicate;
 import reactor.bus.Event;
 import reactor.bus.EventBus;
 
+
 /**
- * 
+ *
  * Listens to Events matching a given selector and matches them to a given
  * predicate
- * 
+ *
  * events are either ignored or repackaged and forwarded with a new selector key
- * 
+ *
  * @author lm
  *
  * @param <T>
  */
 public class ContentFilterEventTap<T> implements Consumer<Event<T>> {
 
-	private final EventBus eventbus;
+private final EventBus eventbus;
 
-	private final String forwardSelectorKey;
+private final String forwardSelectorKey;
 
-	private final Predicate<T> predicate;
+private final Predicate<T> predicate;
 
-	public ContentFilterEventTap(EventBus eventbus, String forwardSelectorKey,  Predicate<T> predicate) {
-		super();
-		this.eventbus = eventbus;
-		this.forwardSelectorKey = forwardSelectorKey;
-		this.predicate = predicate;
-	}
+public ContentFilterEventTap(EventBus eventbus, String forwardSelectorKey,  Predicate<T> predicate) {
+	super();
+	this.eventbus = eventbus;
+	this.forwardSelectorKey = forwardSelectorKey;
+	this.predicate = predicate;
+}
 
 
 
-	@Override
-	public void accept(Event<T> t) {
-
-		if (!predicate.test(t.getData()))
-			return;
-
-		eventbus.notify(forwardSelectorKey, Event.wrap(Objects.requireNonNull(t.getData())));
-	}
+@Override
+public void accept(Event<T> t) {
+	
+	if (!predicate.test(t.getData()))
+		return;
+	
+	eventbus.notify(forwardSelectorKey, Event.wrap(Objects.requireNonNull(t.getData())));
+}
 
 
 }
