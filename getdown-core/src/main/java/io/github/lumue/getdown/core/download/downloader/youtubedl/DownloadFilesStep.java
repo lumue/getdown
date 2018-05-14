e1945ad27171deeff5a64f49466ddbb75f4c5aea
lm <mueller.lutz@gmail.com>
2018-05-15 00:59:16 +0200

    support resuming downloads

--- getdown-core/src/main/java/io/github/lumue/getdown/core/download/downloader/youtubedl/DownloadFilesStep.java
+++ getdown-core/src/main/java/io/github/lumue/getdown/core/download/downloader/youtubedl/DownloadFilesStep.java
@@ -4,6 +4,7 @@ import io.github.lumue.getdown.core.download.job.Progression;
 import io.github.lumue.getdown.core.download.job.ProgressionListener;
 import io.github.lumue.getdown.core.download.task.DownloadFormat;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -17,13 +18,15 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class DownloadFilesStep implements Runnable {
 	
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(DownloadFilesStep.class);
-	private static final int BUFFER_SIZE = 1024;
+	private static final int BUFFER_SIZE = 1024 * 512;
 	private final List<DownloadFormat> selectedFormats;
 	private final String downloadPath;
 	private final ProgressionListener progressionListener;
@@ -54,35 +57,64 @@ public class DownloadFilesStep implements Runnable {
 	private void downloadFile(CloseableHttpClient httpClient, DownloadFormat format) {
 		HttpGet get = new HttpGet(format.getUrl());
 		String filename = downloadPath + File.separator + format.getFilename();
-		format.getHttpHeaders().entrySet().stream()
-				.map(h -> new BasicHeader(h.getKey(), h.getValue()))
-				.forEach(get::addHeader);
 		
-		try (CloseableHttpResponse response = httpClient.execute(get)) {
-			int status = response.getStatusLine().getStatusCode();
-			if (status >= 200 && status < 300) {
-				HttpEntity entity = response.getEntity();
-				
-				// opens an output stream to save into file
-				try (
-						FileOutputStream outputStream = new FileOutputStream(filename);
-						InputStream inputStream = entity.getContent()
-				) {
-					
-					int bytesRead;
-					byte[] buffer = new byte[BUFFER_SIZE];
-					while ((bytesRead = inputStream.read(buffer)) != -1) {
-						LOGGER.debug(bytesRead + " read from " + format.getUrl());
-						outputStream.write(buffer);
-						LOGGER.debug(bytesRead + " written to " + filename);
-						progressionListener.onProgress("downloading " + format.getFilename(), progression.incrementProgress(bytesRead));
-					}
-				}
-			} else {
-				throw new RuntimeException("Unexpected response status: " + status);
+		try {
+			long resumeAt = 0;
+			
+			final File file = new File(filename);
+			if (file.exists()) {
+				resumeAt = Files.size(file.toPath());
 			}
-		} catch (IOException e) {
+			
+			format.getHttpHeaders().entrySet().stream()
+					.map(h -> new BasicHeader(h.getKey(), h.getValue()))
+					.forEach(get::addHeader);
+			
+			if(resumeAt>0L) {
+				get.addHeader("Range", "bytes=" + resumeAt + "-");
+			}
+			
+			try (CloseableHttpResponse response = httpClient.execute(get)) {
+				int status = response.getStatusLine().getStatusCode();
+				if (status >= 200 && status < 300) {
+					HttpEntity entity = response.getEntity();
+					
+					boolean append = false;
+					
+					if (resumeAt > 0L) {
+						if (status != HttpStatus.SC_PARTIAL_CONTENT) {
+							Files.deleteIfExists(new File(filename).toPath());
+							resumeAt = 0L;
+						}
+						else{
+							append=true;
+							progressionListener.onProgress("resuming ",progression.incrementProgress(resumeAt));
+						}
+					}
+				
+					
+					// opens an output stream to save into file
+					try (
+							FileOutputStream outputStream = new FileOutputStream(filename,append);
+							InputStream inputStream = entity.getContent()
+					) {
+						int bytesRead;
+						byte[] buffer = new byte[BUFFER_SIZE];
+						while ((bytesRead = inputStream.read(buffer)) != -1) {
+							LOGGER.debug(bytesRead + " read from " + format.getUrl());
+							outputStream.write(buffer, 0, bytesRead);
+							LOGGER.debug(bytesRead + " written to " + filename);
+							progressionListener.onProgress("downloading " + format.getFilename(), progression.incrementProgress(bytesRead));
+						}
+					}
+				} else {
+					throw new RuntimeException("Unexpected response status: " + status);
+				}
+			}
+		}catch(	IOException e)
+		{
 			throw new RuntimeException(e);
 		}
+		
 	}
 }
