71789ef9579aca326333f42c975a6897440dd2f1
lm <mueller.lutz@gmail.com>
2018-07-26 03:52:52 +0200

    show creation date in task list

--- getdown-ux-console/src/main/polymer/console/src/utils.js
+++ getdown-ux-console/src/main/polymer/console/src/utils.js
@@ -10,3 +10,13 @@ function formatBytes(bytes,decimals) {
 			i = Math.floor(Math.log(bytes) / Math.log(k));
 	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
+
+function formatDateTime(datetimestring){
+	var date = moment(datetimestring, "YYY-MM-DDTHH:mm:ssZ").toDate();
+	var day = date.getDate();
+	var monthIndex = date.getMonth()+1;
+	var year = date.getFullYear();
+	var hours=date.getHours();
+	var minutes=date.getMinutes();
+	return day+"."+monthIndex+"."+" "+hours+":"+minutes
+}
